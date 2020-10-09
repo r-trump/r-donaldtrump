85b8bb3ddf2766998033ae29a6fbefb9dafbee42
Jeffrey Bodin <jeffreybodin@outlook.com>
2020-10-08 19:14:16 -0500

    2020-10-08 - dev - added / adding - octokit -- github api integrations

---
+++ dev/continueous-integration/octokit.js
@@ -0,0 +1,7 @@
+// Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+const octokit = new Octokit({ auth: `personal-access-token123` });
+
+const response = await octokit.request("GET /orgs/:org/repos", {
+  org: "octokit",
+  type: "private",
+});
